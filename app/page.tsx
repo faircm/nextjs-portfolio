4a0e08de77d0ff795dce26b5e13c1c85a439fcff
faircm <91977339+faircm@users.noreply.github.com>
2025-02-26 18:15:17 -0800

    Changed default text, social links in footer, readability

--- app/page.tsx
+++ app/page.tsx
@@ -5,89 +5,58 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        <p>Hi, I'm Chris.</p>
+        <ul>
+          <li>Software Engineer</li>
+          <li>Portfolio-haver (pending)</li>
+          <li>Node.js, Next.js, React, ASP.NET, SQL, MongoDB</li>
+        </ul>
       </main>
       <footer className={styles.footer}>
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://linkedin.com/in/cmfairchild/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
+            className="invert_effect"
             aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/linkedin.svg"
+            alt="Linkedin icon"
+            width={20}
+            height={20}
           />
-          Learn
+          Linkedin
         </a>
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://medium.com/@fair-cm"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
+            className="invert_effect"
             aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+            src="/medium.svg"
+            alt="Medium icon"
+            width={20}
+            height={20}
           />
-          Examples
+          Medium
         </a>
         <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://github.com/faircm"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
+            className="invert_effect"
             aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+            src="/github.svg"
+            alt="Github icon"
+            width={20}
+            height={20}
           />
-          Go to nextjs.org →
+          Github
         </a>
       </footer>
     </div>
